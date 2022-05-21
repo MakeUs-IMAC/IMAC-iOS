ec0507bf0f4c087250a4a8791141cb45ded2b573
YouHojoon <dbghwns66@daum.net>
2022-05-22 06:16:24 +0900

    add: 역할 선택

--- iMAC-Torip/Profile/View/ProfileFormView.swift
+++ iMAC-Torip/Profile/View/ProfileFormView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ProfileFormView: View {
     @State private var nickname = ""
     @State private var contract = ""
+    @State private var roleTag = 0
     @State private var genderTag = 0
-    @State private var gender: Gender = .male
     @State private var age: Age = .teen
     @State private var carType: CarType = .compactCar
+    
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
             VStack(spacing: 20){
@@ -21,7 +23,14 @@ struct ProfileFormView: View {
                     Text("개인 정보 입력").font(.system(size:30, weight: .bold))
                     Spacer()
                 }.padding(.bottom, 30)
-               
+                
+                //역할 선택
+                Picker("", selection: $roleTag){
+                    ForEach(0 ..< UserRole.allCases.count, id:\.self){
+                        Text("\(UserRole.allCases[$0].rawValue)").tag($0)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
                 
                 //공통된 입력
                 TextField("닉네임", text: $nickname)
@@ -29,30 +38,31 @@ struct ProfileFormView: View {
                 TextField("연락처", text: $contract)
                     .textFieldStyle(.roundedBorder)
                 
-                //여행자일 때
-                Picker("", selection: $genderTag){
-                    ForEach(0 ..< Gender.allCases.count, id:\.self){
-                        Text("\(Gender.allCases[$0].rawValue)").tag($0)
+                if UserRole.allCases[roleTag] == UserRole.traveler {
+                    //여행자일 때
+                    Picker("", selection: $genderTag){
+                        ForEach(0 ..< Gender.allCases.count, id:\.self){
+                            Text("\(Gender.allCases[$0].rawValue)").tag($0)
+                        }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    Picker("", selection: $age){
+                        ForEach(0 ..< Age.allCases.count){
+                            Text("\(Age.allCases[$0].rawValue)대")
+                        }
+                    }.pickerStyle(.wheel)
+                        .background(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.15), lineWidth: 1))
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                
-                Picker("", selection: $age){
-                    ForEach(0 ..< Age.allCases.count){
-                        Text("\(Age.allCases[$0].rawValue)대")
-                    }
-                }.pickerStyle(.wheel)
-                    .background(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.15), lineWidth: 1))
-                
-                
-                //기사일 때
-                Picker("", selection: $carType){
-                    ForEach(0 ..< CarType.allCases.count){
-                        Text("\(CarType.allCases[$0].rawValue)")
-                    }
-                }.pickerStyle(.wheel)
-                    .background(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.15), lineWidth: 1))
-                
+                else{
+                    //기사일 때
+                    Picker("", selection: $carType){
+                        ForEach(0 ..< CarType.allCases.count){
+                            Text("\(CarType.allCases[$0].rawValue)")
+                        }
+                    }.pickerStyle(.wheel)
+                        .background(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.15), lineWidth: 1))
+                }
                 Button{
                     
                 }label: {
